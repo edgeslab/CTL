24e76dda9ad474a9532d86aed79467fa687adea0
chris-tran-16 <chris.l.tran.2016@gmail.com>
2018-06-26 19:15:19 -0500

    toy dataset and example code

--- main.py
+++ main.py
@@ -1,50 +1,29 @@
 import pandas as pd
-import time
+import numpy as np
 from causal_tree_learn import *
+from sklearn.model_selection import train_test_split
 
-if __name__ == '__main__':
+asthma = pd.read_csv('data/asthma.txt', delimiter=' ', index_col=None)
 
-    survey = pd.read_csv('data/processed_data.csv')
+asthma.columns = ['physician', 'age', 'sex', 'education', 'insurance','drug coverage', 'severity',
+                  'comorbidity', 'physical comorbidity', 'mental comorbidity', 'satisfaction']
 
-    survey_s = survey[(survey['CF-3'] > 3) | (survey['CF-3'] < 3)]
+y = asthma['satisfaction'].as_matrix()
+treatment = asthma['physician'].as_matrix()
 
-    s_labels = survey_s['CF-3'].as_matrix()
-    treatment = survey_s['date_diff'].as_matrix()
-    treatment_binary = survey_s['date_diff'].copy().as_matrix()
+x = asthma.drop(['satisfaction', 'physician'], axis=1).as_matrix()
 
-    treat_split = 180
+columns = asthma.drop(['satisfaction', 'physician'], axis=1).columns
 
-    treat = treatment_binary > treat_split
-    control = treatment_binary <= treat_split
+y[y == 0] = -1
 
-    treatment_binary[treat] = 1
-    treatment_binary[control] = 0
+treatment[treatment==1] = 0
+treatment[treatment==2] = 1
 
-    g3 = s_labels > 3
-    l3 = s_labels < 3
+# x_train, x_test, y_train, y_test, treat_train, treat_test = train_test_split(x, y, treatment,
+#                                                                                  test_size=0.5, random_state=42)
 
-    s_labels[g3] = -1
-    s_labels[l3] = 1
+x_train, y_train, treat_train = x, y, treatment
 
-    survey_s_dat = survey_s.drop(['CF-3'], axis=1)
-    survey_s_dat = survey_s_dat.drop(['CF-2'], axis=1)  # proxy for sensitvity
-    survey_s_dat = survey_s_dat.drop(['CF-4'], axis=1)
-    survey_s_dat = survey_s_dat.drop(['date_diff'], axis=1)
-
-    survey_s_dat_f = survey_s_dat.copy()
-
-    s_data = survey_s_dat_f.as_matrix()
-
-    x = s_data
-    y = s_labels
-
-    var = survey_s_dat_f.columns
-
-    max_depth = -1
-
-    start = time.time()
-    causal_tree = grow_causal_tree(x, y, treatment_binary, cont=False, min_size=10, max_depth=max_depth, seed=0)
-    prune(causal_tree, alpha=.01)
-    plot_tree(causal_tree, var, "binary")
-    end = time.time()
-    print(end - start)
+causal_tree = grow_causal_tree(x_train, y_train, treat_train, cont=False, min_size=5, max_depth=4)
+plot_tree(causal_tree, columns, "asthma")
