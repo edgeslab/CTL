abc46db8e8ee89a1ded15b9fdacc4e05006e69e0
Chris <chris.tran.2016@gmail.com>
2020-04-09 14:47:48 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ else:
 
 setup(
     name="causal_tree_learn",
-    version="2.19",
+    version="2.20",
     author="Christopher Tran",
     author_email="ctran29@uic.edu",
     description="Python implementation of causal trees with validation",
